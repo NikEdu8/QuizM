fd7dd9a9d555dbc4ffde7a58e6b6d3fa45641c22
NikEdu8 <diligent8@ukr.net>
2017-05-01 10:42:50 +0300

    base

---
+++ QuizAsp/Concrete/DbInitializer.cs
@@ -0,0 +1,30 @@
+﻿using QuizAsp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace QuizAsp.Concrete
+{
+    public class DbInitializer : DropCreateDatabaseAlways<QuizModel>
+    {
+        protected override void Seed(QuizModel db)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Answer answer = new Answer { IsCorrect = true, IdQuestion = 1 + i, Text = "Answer1", Id = 1 + i };
+                var answers = new List<Answer>();
+                Test test = new Test { Id = 1 + i, Caption = "Test1" +i, Timer = new TimeSpan(0, 5, 0) };
+                Question question = new Question { Text = "A?", Answer = answers, Id = 1 + i, Test = test, TestId = test.Id };
+                db.Answer.Add(answer);
+                db.Test.Add(test);
+                db.Question.Add(question);
+            }
+            db.SaveChanges();
+
+            //Seed(db);
+            //   db.Question.Add();
+        }
+    }
+}
---
+++ QuizAsp/Scripts/AddQuestion.js
@@ -0,0 +1,69 @@
+﻿
+
+
+
+var i = 1;
+
+
+
+
+function get() {
+
+
+    var articleDiv = document.getElementById('Question[0].Text');
+
+    var newArticleDiv = articleDiv.cloneNode(true);
+
+    newArticleDiv.setAttribute("name", "Question[" + i + "].Text");
+
+    var elemText = document.createTextNode("Question");
+    var answers = document.createTextNode("Answers");
+    var elemt = document.createElement("br");
+
+    var br = document.createElement("br");
+    var br1 = document.createElement("br");
+
+
+    f.appendChild(elemText);
+    f.appendChild(br);
+
+
+    f.appendChild(newArticleDiv);
+    f.appendChild(elemt);
+    f.appendChild(answers);
+    f.appendChild(br1);
+
+
+    var checkbox = document.createElement("input");
+
+    var dropid = document.getElementById('Question[0].Answer[0].Text');
+
+    var j = 0;
+
+    for (j ; j < 4; j++) {
+
+        var c = checkbox.cloneNode(true);
+        var nD = dropid.cloneNode(true);
+        c.setAttribute("id", "Question_" + i + "__Answer_" + j + "__IsCorrect");
+        c.setAttribute("name", "Question[" + i + "].Answer[" + j + "].IsCorrect");
+        c.setAttribute("type", "checkbox");
+        c.setAttribute("value", "true");
+
+        nD.setAttribute("id", "Question[" + i + "].Answer[" + j + "].Text");
+        nD.setAttribute("name", "Question[" + i + "].Answer[" + j + "].Text");
+
+        var elem = document.createElement("br");
+
+        f.appendChild(nD);
+        f.appendChild(c);
+        f.appendChild(elem);
+    }
+
+
+
+
+    i++;
+
+
+
+}
